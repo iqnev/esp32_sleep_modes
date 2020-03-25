35782114b1d99d34688a58fdea88a72da4b53882
fortran <bgfortran@gmail.com>
2020-03-25 20:33:05 +0200

    add deep sleep demo code

---
+++ deep_sleep_demo_main.c
@@ -0,0 +1,50 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_sleep.h"
+
+static RTC_DATA_ATTR int counter = 0;
+
+void app_main(void)
+{
+    int sleep_time_ms = 1500;
+    switch (esp_sleep_get_wakeup_cause()) {
+        case ESP_SLEEP_WAKEUP_EXT1: {
+            uint64_t wakeup_pin_mask = esp_sleep_get_ext1_wakeup_status();
+            if (wakeup_pin_mask != 0) {
+                int pin = __builtin_ffsll(wakeup_pin_mask) - 1;
+                printf("Wake up from GPIO %d\n", pin);
+            } else {
+                printf("Wake up from GPIO\n");
+            }
+            break;
+        }
+        case ESP_SLEEP_WAKEUP_TIMER: {
+            printf("Wake up from timer. Time spent in deep sleep: %d\n", sleep_time_ms);
+            printf("Wake up from timer. The value of counter is: %d\n", counter);
+            break;
+        }
+        case ESP_SLEEP_WAKEUP_TOUCHPAD: {
+            printf("Wake up from touch on pad %d\n", esp_sleep_get_touchpad_wakeup_status());
+            break;
+        }
+        case ESP_SLEEP_WAKEUP_UNDEFINED:
+        default:
+            printf("Not a deep sleep reset\n");
+    }
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    const int wakeup_time_sec = 20;
+    printf("Enabling timer wakeup, %ds\n", wakeup_time_sec);
+    esp_sleep_enable_timer_wakeup(wakeup_time_sec * 1000000);
+
+    printf("Entering deep sleep\n");
+    counter++;
+    esp_deep_sleep_start();
+}
